f9cb15aeb1f275de34f2a7c1caaf69fe374c9627
Nick <NICHOLASACLARK5@GMAIL.COM>
2025-07-14 12:59:18 -0500

    Add files via upload

---
+++ scale_rotate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+input_dir = os.path.join(os.getcwd(), 'icons')
+output_dir = os.path.join(os.getcwd(), 'icons_fixed')
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for filename in os.listdir(input_dir):
+    if filename.startswith('ic_'):
+        file_path = os.path.join(input_dir, filename)
+        new_filename = os.path.splitext(filename)[0] + '.jpeg'
+        new_file_path = os.path.join(output_dir, new_filename)
+
+        with Image.open(file_path) as img:
+            new_image = img.rotate(270).resize((128, 128)).convert('RGB').save(new_file_path, "JPEG")
